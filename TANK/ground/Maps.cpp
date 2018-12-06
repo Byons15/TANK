f32cdf5da15f3144155f2d904c62c9e9af2dc425
ppuding <38817866+ppuding@users.noreply.github.com>
2018-12-06 18:48:36 +0800

    2018年12月6日18:48:34

--- TANK/ground/Maps.cpp
+++ TANK/ground/Maps.cpp
@@ -12,19 +12,27 @@ int Maps::loadMaps(int level)
 
 	clearMap();
 
+	//导入数据。
 	std::string groupName = "level " + std::to_string(level);
 	std::vector<std::string> data;
 	if (fileLoad("level", groupName, &data) != MAP_SIZE)
 		return -1;
 	
+	
+	//读取数据并构造地图。
 	size_t enemyRefreshIndex = 0;
 	for (int ln = 0; ln != MAP_SIZE; ++ln) {
 		std::istringstream is(data[ln]);
 		for (int col = 0; col != MAP_SIZE; ++col) {
 			is >> m_map[col][ln];
+
+			//导入的数据标识中-1表示P1刷新点， -2表示P2刷新点， -3表示敌军刷新点。
 			if (m_map[col][ln] < 0) {
 				m_map[col][ln] = 0;
 
+				//将坐标的中的标识赋0，并保存这个敌军刷新点。说一下就是敌军刷新点最多有且必须有3个，多了溢出报错， 
+				//赋0表示这个点是空的。
+				//之后的两个if语句是保存P1 P2刷新点的。
 				if (m_map[col][ln] == -3) 
 					m_enemyRefreshPoint[enemyRefreshIndex++] = { col, ln };
 
@@ -37,7 +45,7 @@ int Maps::loadMaps(int level)
 		}
 	}
 	
-
+	
 
 	return 0;
 }
--- TANK/ground/Maps.h
+++ TANK/ground/Maps.h
@@ -12,6 +12,12 @@ public:
 	int loadMaps(int level);
 	int operator()(int x, int y);
 	int setTerrain(int x, int y, uint32_t terrainIndex = 0);
+	const SDL_Point &enemyRefreshPoint(int index) {
+		return m_enemyRefreshPoint[index];
+	}
+	const SDL_Point &alliesRefreshPoint(int index) {
+		return m_alliesRefreshPoint[index];
+	}
 	void clearMap();
 	~Maps();
 private:
